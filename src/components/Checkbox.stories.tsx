ad80dc61dbe54c97c3b5da52722e7de7f7879c02
monezero <monezerolindo@gmail.com>
2023-03-19 20:38:45 -0300

    Components

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,11 @@
+import { CheckBox, CheckBoxProps } from "./Checkbox";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/CheckBox",
+  component: CheckBox,
+  args: {},
+  argTypes: {},
+} as Meta<CheckBoxProps>;
+
+export const Default: StoryObj<CheckBoxProps> = {};
